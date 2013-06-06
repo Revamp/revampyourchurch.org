515c3956158e2399a44c0f959c1ff8f95aeb7f79
Joshua Smith <joshua-smith@verizon.net>
2013-06-06 18:03:30 -0400

    Set certain fields as required

--- Contact/index.php
+++ Contact/index.php
@@ -12,10 +12,10 @@
   <form>
     <div class="box"> 
       <h1>Contact Us:</h1>
-      <input type="text" name="organization" id="organization" placeholder="Revamp" />
-      <input type="email" name="email" id="email" placeholder="revamp@revamp.co.nf" />
-      <input type="text" name="subject" id="subject" placeholder="Service Inquiry" />
-      <textarea name="feedback" id="feedback" placeholder="I am contacting you because..." ></textarea>
+      <input type="text" name="organization" id="organization" placeholder="Revamp" required />
+      <input type="email" name="email" id="email" placeholder="revamp@revamp.co.nf" required />
+      <input type="text" name="subject" id="subject" placeholder="Service Inquiry" required />
+      <textarea name="feedback" id="feedback" placeholder="I am contacting you because..." required ></textarea>
       <div class="buttons">
         <input type="submit" value="Contact" />
       </div>
